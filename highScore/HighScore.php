ae4bfdb8da7275e3bf0a24e563380b4ed46622fd
Prashant <Princerocknroll786@gmail.com>
2017-05-05 17:33:58 +0530

    highscore System Complete

--- highScore/HighScore.php
+++ highScore/HighScore.php
@@ -4,28 +4,27 @@ require "../db.php";
 if(isset($_GET['gameId']))
 $gameId=$_GET['gameId'];
 else
-$gameId=1;//put the game id to show the highscore@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+$gameId=2;//put the game id to show the highscore@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 $result=$Scoring->query("SELECT `scoreType` from game where `gameId`=$gameId");
 $row = $result->fetch_assoc();
 $order=$row["scoreType"]?"ASC":"DESC";//tell scorring type
 
 $result=$Scoring->query("SELECT `userName`,`score`,`date`, `scoreType`,CASE
-WHEN P.`genderId`=0 THEN 'Male'
-WHEN P.`genderId`=1 THEN 'Female'
-WHEN P.`genderId`=2 THEN 'Others'
+WHEN P.`genderId`=0 THEN 'male'
+WHEN P.`genderId`=1 THEN 'female'
+WHEN P.`genderId`=2 THEN 'others'
 END as gender from highscore H LEFT JOIN `players` P ON h.`UserId`=P.
 `UserId` LEFT JOIN `game` G on H.`GameId`=G.`GameId` 
-where H.`GameId`=$gameId ORDER BY `score` $order");
+where H.`GameId`=$gameId ORDER BY `score` $order LIMIT 7");
 if ($result && $result->num_rows > 0)
 for($i=1;$row = $result->fetch_assoc();$i++){
 //tell the user gender
 echo"<tr ondblclick='specificEntry(this)' class='".$row["gender"]."'>
 <td>".$row["userName"]."</td>
 <td>".$row["score"]."</td>
-<td>".$row["scoreType"]."</td>
-<td>".date("d-M-y", strtotime($row["date"])).strftime('%I:%M %p',strtotime($row["date"]))."</td>
+<td>".date("d-M-y", strtotime($row["date"]))."</td>
 </tr>
-</br>";}
+";}
 else
 echo"<tr>
     <td>No Record</td>
--- highScore/HighScorepage.php
+++ highScore/HighScorepage.php
@@ -1,49 +1,142 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-        <link rel="icon" href="../images/logo.png">
-        <link rel="stylesheet" href="../head-foot.css">
-        <script src="../allFileJs.js"></script>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="icon" href="../images/logo.png">
+    <link rel="stylesheet" href="../head-foot.css">
+    <script src="../allFileJs.js"></script>
+    <style>
+      .container {
+            top: 16vh;
+            position: fixed;
+            width: 100%;
+            flex-direction: column;
+        }
+        
+        #control{
+            display: grid;
+            text-align: center;
+            grid-column-gap: 20px;
+            grid-row-gap:40px;
+            grid-template-columns:repeat(3,auto);
+
+        }
+        #control div img{
+            height: 120px;
+            border-radius: 10px;
+            border:3px solid var(--tColor);
+        }
+        #control div img[class="active"]{
+            box-shadow:0px 0px 15px 5px skyblue;
+        }
+        .data{
+            box-shadow:0px 0px 50px 15px black;
+            border-radius: 20px;
+            padding: 5px;
+        }
+        
+        .data tr{
+            color: var(--tColor);
+            font-family: cursive;
+        }
+        .data td{
+            text-align: center;
+            border-radius: 40%;
+            border-bottom: 2px solid var(--tColor);
+            padding: 5px 10px;
+            font-size: 1.8em;
+        }
+        .others td{
+            border-bottom: 2px solid green;
+        }
+        .female td{
+            border-bottom: 2px solid red;
+        }
+        
+        @media (max-width:700px){
+            #control{
+            grid-column-gap: 10px;
+            grid-row-gap:20px;
+            }
+            #control div img{
+                height: 80px;
+            }
+            .data td{
+                padding: 2px 5px;
+                font-size: 1.5em;
+            }
+        }
+        @media (max-width:500px){
+            #control{
+            grid-column-gap: 5px;
+            grid-row-gap:10px;
+            }
+            #control div img{
+                height: 70px;
+            }
+            .data td{
+                font-size: 1.3em;
+            }
+        }
+        
+     </style>
 </head>
+
 <body>
-<header>
+    <header>
         <nav class="flex" id="navbar">
-            <div class=""><a href="#"><img src="../images/logo.png"></a></div><!--go to highScore page -->
+            <div class=""><a href="#"><img src="../images/logo.png"></a></div>
+            <!--go to highScore page -->
             <div id="title"><a href="#">Game-Troupe</a></div>
         </nav>
     </header>
-    <select id="flavours"> 
-<?php
-require "../db.php";
-$result=$Scoring->query("SELECT gameName,gameId from game order by gameId");
-if ($result && $result->num_rows > 0)
-for($i=1;$row = $result->fetch_assoc();$i++){
-    echo "<option value='".$row['gameId']."' >".$row['gameName']."</option>
-    ";
-}
-?>
-    </select>
-    <div id="score">
-        <?php require "highScore.php";?>
+    
+    <div class="container flex">
+        <div id="control">
+            <div id="game1" onclick="currentGame(0,1)"><img src="../images/Game1.png" alt="game"></div>
+            <div id="game2" onclick="currentGame(1,2)"><img class="active" src="../images/logo.png" alt="game"></div>
+            <div id="game3" onclick="currentGame(2,3)"><img src="../images/Game1.png" alt="game"></div>
+            <div id="game4" onclick="currentGame(3,4)"><img src="../images/logo.png" alt="game"></div>
+            <div class="data flex">
+                <table>
+                    <thead>
+                        <tr>
+                            <td>Name</td>
+                            <td>Score</td>
+                            <td>Date</td>
+                        </tr>
+                    </thead>
+                    <tbody id="dataContent">
+                           <?php require "highScore.php"?>
+                    </tbody>
+                </table>
+            </div>
+            <div id="game5" onclick="currentGame(5,5)"><img src="../images/logo.png" alt="game"></div>
+        </div>
     </div>
     <footer class="flex" id="footer">
-        <div><a href="../log/signUp.html" class="button">Sign Up</a></div><!--go to highScore page -->
+        <div><a href="../log/signUp.html" class="button">Sign Up</a></div>
+        <!--go to highScore page -->
         <div><a href="../log/logIn.html" class="button">Login</a></div><!-- go to login up page -->
     </footer>
 </body>
+
 </html>
 <script>
-      document.addEventListener('DOMContentLoaded', () => {
-        document
-          .getElementById('flavours')
-          .addEventListener('input', ev=>{
-        let select = ev.target; //document.getElementById('flavours');
-        url="highScore.php?gameId="+select.value;
-        loadDoc(url,"score");
-      });
-      });
+    var active=1;
+    var img=document.querySelector("#control").children;
+
+    function currentGame(nextLoc,gameId){
+    console.log("Data Update Game no."+gameId);
+    img[active].firstChild.classList.remove("active");
+    img[nextLoc].firstChild.classList.add("active");
+    active=nextLoc;
+    url = "highScore.php?gameId=" + gameId;
+    loadDoc(url, "dataContent");
+    }
 </script>
+</script> 
