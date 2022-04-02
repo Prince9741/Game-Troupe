7d3c8343d3f274c660796aec5eb9ca9abe9dcdef
Prashant <Princerocknroll786@gmail.com>
2022-04-02 22:15:15 +0530

    highscore managing

--- highScore/HighScore.php
+++ highScore/HighScore.php
@@ -1,24 +1,25 @@
     
 <?php // joing query to fetch the data
 require "../db.php";
-if(isset($_GET['e']))
-$gameId=$_GET['e'];
+if(isset($_GET['gameId']))
+$gameId=$_GET['gameId'];
 else
-$gameId=3;//put the game id to show the highscore@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+$gameId=1;//put the game id to show the highscore@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 $result=$Scoring->query("SELECT `scoreType` from game where `gameId`=$gameId");
 $row = $result->fetch_assoc();
-$order=$row["scoreType"];
-echo $order=$order?"ASC":"DESC";//tell scorring type
+$order=$row["scoreType"]?"ASC":"DESC";//tell scorring type
 
 $result=$Scoring->query("SELECT `userName`,`score`,`date`,`gameName`, `scoreType`,CASE
 WHEN P.`genderId`=0 THEN 'Male'
 WHEN P.`genderId`=1 THEN 'Female'
 WHEN P.`genderId`=2 THEN 'Others'
-END as gender from highscore H LEFT JOIN `players` P ON h.`UserId`=P.`UserId` LEFT JOIN `game` G on H.`GameId`=G.`GameId` where H.`GameId`=$gameId ORDER BY `score` $order");
+END as gender from highscore H LEFT JOIN `players` P ON h.`UserId`=P.
+`UserId` LEFT JOIN `game` G on H.`GameId`=G.`GameId` 
+where H.`GameId`=$gameId ORDER BY `score` $order");
 if ($result && $result->num_rows > 0)
 for($i=1;$row = $result->fetch_assoc();$i++){
-$gender=$row["gender"];//tell the user gender
-echo"<tr ondblclick='specificEntry(this)' class='".$gender."'>
+//tell the user gender
+echo"<tr ondblclick='specificEntry(this)' class='".$row["gender"]."'>
 <td>".$row["userName"]."</td>
 <td>".$row["score"]."</td>
 <td>".$row["gameName"]."</td>
@@ -35,5 +36,5 @@ echo"<tr>
     <td>No Record</td>
 </tr>";  
     //print all the entry
-    ?> 
+?> 
 
--- highScore/HighScorepage.php
+++ highScore/HighScorepage.php
@@ -8,14 +8,14 @@
         <script src="../allFileJs.js"></script>
 </head>
 <body>
-    <select> 
+    <select id="flavours"> 
 <?php
 require "../db.php";
 $gameId=3;//put the game id to show the highscore@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-$result=$Scoring->query("SELECT gameName,gameId from game");
+$result=$Scoring->query("SELECT gameName,gameId from game order by gameId");
 if ($result && $result->num_rows > 0)
 for($i=1;$row = $result->fetch_assoc();$i++){
-    echo "<option onclick='changeHighscore(".$row['gameId'].")' >".$row['gameName']."</option>
+    echo "<option value='".$row['gameId']."' >".$row['gameName']."</option>
     ";
 }
 ?>
@@ -26,9 +26,15 @@ for($i=1;$row = $result->fetch_assoc();$i++){
 </body>
 </html>
 <script>
-function changeHighscore(e){
-    console.log("hello");
-    // e="highScore.php?e="+e;
-    // loadDoc(e,"score");
-}
+      document.addEventListener('DOMContentLoaded', () => {
+        document
+          .getElementById('flavours')
+          .addEventListener('input', handleSelect);
+      });
+
+      function handleSelect(ev) {
+        let select = ev.target; //document.getElementById('flavours');
+        url="highScore.php?gameId="+select.value;
+        loadDoc(url,"score");
+      }
 </script>
