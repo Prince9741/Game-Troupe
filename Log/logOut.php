6580a290a4d7b585efde1fb134be62f63d0ac164
Prashant <Princerocknroll786@gmail.com>
2022-04-23 16:00:16 +0530

    game 2 structure complete

--- Log/logOut.php
+++ Log/logOut.php
@@ -4,6 +4,6 @@ session_start();
 session_unset();
 session_destroy();
 session_start();
-$_SESSION['msg']="Logout Successfull";
+$_SESSION['msg']='<div class="msg msgSuccess">Logout Successfull</div>';
 header("location:logIn.php");
 ?>
